4d5f26f2f8ba8e01193d3005ad63863c2e035b40
Lovely Shane Ong <134838637+xienshane@users.noreply.github.com>
2025-11-12 16:30:50 +0800

    telephone scene sounds

--- dialogues/telephone_dialogue.gd
+++ dialogues/telephone_dialogue.gd
@@ -1,11 +1,31 @@
 extends Node3D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Connect the signal first
+	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
 	
 	# Start the ending animation
 	$AnimationPlayer.play("telephone")
+
+func _on_animation_finished(anim_name: String):
+	if anim_name == "telephone":
+		create_blackout()
+
+func create_blackout():
+	var blackout_layer = CanvasLayer.new()
+	blackout_layer.layer = 100
 	
-	# Wait for the entire animation to complete (60 seconds)
-	await get_tree().create_timer(60.0).timeout
+	var black_screen = ColorRect.new()
+	black_screen.color = Color.TRANSPARENT
+	black_screen.size = get_viewport().size  # Use viewport size
+	black_screen.anchor_left = 0
+	black_screen.anchor_top = 0
+	black_screen.anchor_right = 1
+	black_screen.anchor_bottom = 1
 	
+	blackout_layer.add_child(black_screen)
+	add_child(blackout_layer)
+	
+	# Fade to black over 3 seconds
+	var tween = create_tween()
+	tween.tween_property(black_screen, "color", Color.BLACK, 3.0)
