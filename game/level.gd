476422e289ac86c74e6a5fa594a52dbc28b700b0
Ruhmer Jairus Espina <ruhmerjairus.espina@cit.edu>
2025-11-18 13:01:33 +0800

    changes in the lighting

--- game/level.gd
+++ game/level.gd
@@ -1,8 +1,13 @@
 extends Node3D
 
+@onready var lights = $House/doors/NavigationRegion3D/Lights
+
 func _ready():
 	# Wait one frame so we are 100% sure the level and all its children exist
+	lights.visible = false
 	await get_tree().process_frame
+	
 	
 	# Now, we tell Global "Here is the level node, find the tasks inside me."
 	Global.start_night_logic(self)
+	
